eb93a6b044c73a057e651c9af5670b200bdcedf5
Minestone4306 <78621484+dcjvliet@users.noreply.github.com>
2023-12-30 21:03:06 -0500

    Update menu.py
    Updated main file to align with changes for a config file.

--- DownloadThis/menu.py
+++ DownloadThis/menu.py
@@ -6,6 +6,8 @@ import multitasking
 import keybinds
 import late_game
 import top_times
+import config_reader
+import save_state
 
 
 def mainMenu():
@@ -20,8 +22,18 @@ def mainMenu():
     pygame.font.Font.set_underline(font, True)
     pygame.font.Font.set_underline(small_font, True)
     pygame.font.Font.set_underline(big_font, True)
-    key = ']'
-    ide = 0
+    config = config_reader.configRead()
+    try:
+        filler = config[7]
+        filled = True
+    except IndexError:
+        filled = False
+    if filled:
+        key = config[7][0]
+        ide = config[7][1]
+    else:
+        key = ']'
+        ide = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -29,30 +41,87 @@ def mainMenu():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 x, y = pygame.mouse.get_pos()
                 if 25 < x < 794 and 25 < y < 131:
-                    num = recognition.numStartingWords()
-                    starting_words = recognition.startingWords(num)
-                    recognition.wordle(starting_words, num, key)
-                    break
+                    if filled:
+                        use_config = save_state.saveState()
+                        if not use_config:
+                            num = recognition.numStartingWords()
+                            starting_words = recognition.startingWords(num)
+                            recognition.wordle(starting_words, num, key)
+                            break
+                        else:
+                            starting_words = config[0]
+                            num = config[1]
+                            recognition.wordle(starting_words, num, key)
+                            for i in range(6 - num):
+                                config[0].pop(num)
+                            for i in range(6 - num):
+                                config[0].append('')
+                            break
+                    else:
+                        num = recognition.numStartingWords()
+                        starting_words = recognition.startingWords(num)
+                        recognition.wordle(starting_words, num, key)
+                        break
                 elif 1271 < x < 1895 and 25 < y < 131:
-                    num = recognition.numStartingWords()
-                    starting_words = recognition.startingWords(num)
-                    starting.starting(starting_words, num, key)
-                    break
+                    if filled:
+                        use_config = save_state.saveState()
+                        if not use_config:
+                            num = recognition.numStartingWords()
+                            starting_words = recognition.startingWords(num)
+                            starting.starting(starting_words, num, key)
+                            break
+                        else:
+                            starting_words = config[2]
+                            num = config[3]
+                            starting.starting(starting_words, num, key)
+                            for i in range(6 - num):
+                                config[2].append('')
+                            break
+                    else:
+                        num = recognition.numStartingWords()
+                        starting_words = recognition.startingWords(num)
+                        starting.starting(starting_words, num, key)
+                        break
                 elif 25 < x < 958 and 949 < y < 1055:
                     accuracy.blahblah()
                     break
                 elif 1130 < x < 1895 and 949 < y < 1055:
-                    num = recognition.numStartingWords()
-                    starting_words = recognition.startingWords(num)
-                    multitasking.multitasking(starting_words)
+                    if filled:
+                        use_config = save_state.saveState()
+                        if not use_config:
+                            num = recognition.numStartingWords()
+                            starting_words = recognition.startingWords(num)
+                            multitasking.multitasking(starting_words)
+                        else:
+                            starting_words = config[4]
+                            multitasking.multitasking(starting_words)
+                    else:
+                        num = recognition.numStartingWords()
+                        starting_words = recognition.startingWords(num)
+                        multitasking.multitasking(starting_words)
                 elif 25 < x < 195 and 487 < y < 593:
                     pygame.quit()
                 elif 1576 < x < 1895 and 487 < y < 593:
                     ide, key = keybinds.setKeybind(ide, key)
                 elif 25 < x < 837 and 131 < y < 203:
-                    num = recognition.numStartingWords()
-                    starting_words = recognition.startingWords(num)
-                    late_game.lateGame(starting_words, num, key)
+                    if filled:
+                        use_config = save_state.saveState()
+                        if not use_config:
+                            num = recognition.numStartingWords()
+                            starting_words = recognition.startingWords(num)
+                            late_game.lateGame(starting_words, num, key)
+                        else:
+                            starting_words = config[5]
+                            num = config[6]
+                            late_game.lateGame(starting_words, num, key)
+                            for i in range(12 - num):
+                                config[5].pop(num)
+                            for i in range(6 - num):
+                                config[5].append('')
+                    else:
+                        num = recognition.numStartingWords()
+                        starting_words = recognition.startingWords(num)
+                        late_game.lateGame(starting_words, num, key)
                 elif 663 < x < 1257 and 462 < y < 618:
                     top_times.topTimes(ide)
                     break
